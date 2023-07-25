c2bf896152ec5ea10c5aa30ac7f3485d4bed59fb
deva <devadharsan913@gmail.com>
2023-07-25 19:18:02 +0530

    Updated Icon and Contacts.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         </div>
         <div className="flex justify-center items-center">
           <form
-            action="https://getform.io/f/31313319-e886-40c5-af98-21b4317d9014"
+            action="https://getform.io/f/48f425a5-1a8e-4626-a009-60935f735497"
             method="POST"
             className="flex flex-col w-full md:w-1/2"
           >
