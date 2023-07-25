1d586efc4b441521cd640d353d0b31b82d116159
deva <devadharsan913@gmail.com>
2023-07-25 22:05:36 +0530

    ....

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,33 +1,28 @@
 import React, {useState} from 'react';
 import {FaBars} from "react-icons/fa";
-import home from "./Home";
-import about from "./About";
-import projects from "./Projects";
-import skills from "./Skills";
-import contact from "./Contact";
-
+import {Link} from "react-scroll";
 const NavBar = () => {
     const[nav,setNav] = useState(false);
     const links = [
         {
             id: 1,
-            link: home(),
+            link: "home",
         },
         {
             id: 2,
-            link: about(),
+            link: "about",
         },
         {
             id: 3,
-            link: projects(),
+            link: "projects",
         },
         {
             id: 4,
-            link: skills(),
+            link: "skills",
         },
         {
             id: 5,
-            link: contact(),
+            link: "contact",
         },
     ];
 
@@ -39,7 +34,7 @@ const NavBar = () => {
             <ul className="hidden md:flex">
                 {links.map(({id, link}) => (
                 <li key = {id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
-                {link}
+                <Link to = {link} smooth duration={500}>{link}</Link>
                 </li>
                 ))}
             </ul>
